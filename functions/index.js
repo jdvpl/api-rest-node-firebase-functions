9e096381bb7c70b105b734dcf2c1eb89530e8620
JDVPL <juanda554242@gmail.com>
2022-01-14 16:30:14 -0500

    rendpoint finalizados

--- functions/index.js
+++ functions/index.js
@@ -12,14 +12,75 @@ admin.initializeApp({
 const db=admin.firestore();
 
 app.get("/hola", (req, res)=>{
-  return res.status(200).json({message: "Hola insectos"});
+  return res.status(200).json({message: "Api rest con node y firebase"});
 });
 
+// crear producto
 app.post("/api/products", async(req, res) => {
- await db.collection("products")
-      .doc()
-      .create({name: req.body});
-    return res.status(204).json({mensaje: "producto agregado"})
+ try {
+    await db.collection("products")
+    .doc()
+    .set(req.body);
+    res.status(200).json({msg: "se agrego correctamente"})
+ } catch (error) {
+     console.log(error)
+     return res.status(500).send(error)
+ }
 });
+
+// obtener producto por id
+app.get("/api/product/:id",(req,res)=>{
+    (async ()=>{
+        try {
+            const doc= db.collection("products").doc(req.params.id)
+            const item=await doc.get();
+            const response=item.data();
+            return res.status(200).json(response)
+          } catch (error) {
+            console.log(error)
+            return res.status(500).send(error)
+          }
+    })();
+})
+
+app.get("/api/products",async(req, res)=>{
+    try {
+        const query=db.collection("products");
+    const querySnapshot=await query.get();
+    const respose=querySnapshot.docs.map(doc =>({
+        id:doc.id,
+        nombre: doc.data().nombre,
+        precio:doc.data().precio,
+        cantidad: doc.data().cantidad
+
+    }));
+    return res.status(200).json(respose)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error)
+    }
+});
+// elete
+app.delete("/api/product/:id", async(req, res)=>{
+    try {
+       const doc= db.collection("products").doc(req.params.id)
+        await doc.delete();
+        res.status(200).json({msg: "se elminino correctamente"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error)
+    }
+})
+
+app.put("/api/product/:id", async(req, res)=>{
+    try {
+      const document= db.collection("products").doc(req.params.id);
+      await document.update(req.body)
+      return res.status(200).json({msg: "Se actualizo correctamente el producto "+req.body.nombre})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error)
+    }
+})
 exports.app=functions.https.onRequest(app);
 
